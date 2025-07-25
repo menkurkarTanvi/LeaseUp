30839fc20442afa5a2d3c152d4c81f83581ee1e3
Aryahi Nittur <aryahi.nittur@gmail.com>
2025-07-24 19:32:30 -0500

    Set up basic leases page, leases backend, and added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import MapPage from './MapPage';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MapPage from './MapPage';
+import LeasesPage from './LeasesPage';
+import Navbar from './Navbar';
 
 function App() {
-    return (
-        <MapPage />
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MapPage />} />
+        <Route path="/leases" element={<LeasesPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
