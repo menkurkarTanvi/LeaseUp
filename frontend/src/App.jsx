2e37e73d0c1681d6ed1a9429314d89877d55bbf1
Yasashwini Napa <yasashwini.napa@gmail.com>
2025-07-25 14:04:54 -0400

    resolved conflicts and changed to jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@
 import ApartmentForm from "./components/FormComponents/ApartmentForm";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MapsPage from './components/MapsPage';
+import LeasesPage from './LeasesPage';
+import Navbar from './Navbar';
+
+
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<ApartmentForm/>}></Route>
         <Route path="/maps" element={<MapsPage/>}></Route>
-
-      
+        <Route path="/leases" element={<LeasesPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/MapsPage.jsx
+++
@@ -1,7 +0,0 @@
-export default function MapsPage() {
-
-    return (
-
-        <p> on maps page!</p>
-    )
-}
