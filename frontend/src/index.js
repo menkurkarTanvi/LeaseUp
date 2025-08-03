7405dfc8683a470ddf06989f08bd36c3ec6d94d5
Yasashwini Napa <yasashwini.napa@gmail.com>
2025-08-03 17:22:11 -0400

    really resolved now lol

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 const theme = createTheme();
 const root = ReactDOM.createRoot(document.getElementById('root'));
